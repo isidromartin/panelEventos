a13ce7a4e0c8a6d721aa85556ceff425c580e2a2
Isidro <i.martin@pristok.com>
2025-03-23 01:19:25 +0100

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
-import tailwindcss from "@tailwindcss/vite";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  vite: {
-    plugins: [tailwindcss()],
+  adapter: vercel(),
+  integrations: [tailwind()],
+  experimental: {
+    assets: true,
   },
 });
